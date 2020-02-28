318cff08bcc3ac857f832ac2be723cbc167a763a
Luciano <luciano.fndz@gmail.com>
2020-02-28 02:06:32 -0300

    0011 - rotunda - fiexed identation change 4 spaces for 2 on visualcode

--- urlLogic.js
+++ urlLogic.js
@@ -1,57 +1,57 @@
 getInputValues = () => {
-    //Cases of test
-    //https://www.aulafacil.com/:cursos/frances/:a1-t1056
-    //https://www.aulafacil.com/cursos/frances?sort=desc&limit=10
+  //Cases of test
+  //https://www.aulafacil.com/:cursos/frances/:a1-t1056
+  //https://www.aulafacil.com/cursos/frances?sort=desc&limit=10
+  
+  //Get input values
+  const urlFormat = document.getElementById("form-format").value;
+  const urlInput = document.getElementById("form-value").value;
+
+  if (urlFormat && urlInput) {
+
+    //Filter URl format by / content
+    var parserUrlFormat = document.createElement("a"); 
+    parserUrlFormat.href = urlFormat; 
+    var pathArray = parserUrlFormat.pathname.split('/');
+
+    //Array values
+    var parserUrlInstance = document.createElement("a"); 
+    parserUrlInstance.href = urlInput;
+    var valuesArray = parserUrlInstance.pathname.split('/');
     
-    //Get input values
-    const urlFormat = document.getElementById("form-format").value;
-    const urlInput = document.getElementById("form-value").value;
-
-    if (urlFormat && urlInput) {
-
-        //Filter URl format by / content
-        var parserUrlFormat = document.createElement("a"); 
-        parserUrlFormat.href = urlFormat; 
-        var pathArray = parserUrlFormat.pathname.split('/');
-
-        //Array values
-        var parserUrlInstance = document.createElement("a"); 
-        parserUrlInstance.href = urlInput;
-        var valuesArray = parserUrlInstance.pathname.split('/');
-        
-        //Delete white spaces, colon and constant variables
-        var arrayKeys = [];
-        var arrayValues = [];
-        for (i = 0; i < pathArray.length; i++) {
-            if (pathArray[i] !=='' && pathArray[i].charAt(0) === ':') {
-                arrayKeys.push(pathArray[i].substring(1));
-                if( valuesArray[i] && valuesArray[i] !=='' ) {
-                    arrayValues.push(valuesArray[i]);
-                }
-                else {
-                    arrayValues.push(null);
-                }
-            }
+    //Delete white spaces, colon and constant variables
+    var arrayKeys = [];
+    var arrayValues = [];
+    for (i = 0; i < pathArray.length; i++) {
+      if (pathArray[i] !=='' && pathArray[i].charAt(0) === ':') {
+        arrayKeys.push(pathArray[i].substring(1));
+        if( valuesArray[i] && valuesArray[i] !=='' ) {
+          arrayValues.push(valuesArray[i]);
         }
-
-        var variablesObject = createObject(arrayKeys, arrayValues);
-
-        //Queries
-        if(parserUrlInstance.search) {
-            var queries = parserUrlInstance.search;
-            var queriesObject = CreateQueryObject(queries);
-            var mergedObject = {...variablesObject, ...queriesObject};
-            console.log("object", mergedObject);  
+        else {
+          arrayValues.push(null);
         }
-        else{
-          console.log("object", variablesObject);
-        }
-        
-        alert("Check console log to inspect printed object!");    
+      }
     }
-    else {
-      alert("You must fill both boxes and provide a valid url domain .com!");    
+
+    var variablesObject = createObject(arrayKeys, arrayValues);
+
+    //Queries
+    if(parserUrlInstance.search) {
+      var queries = parserUrlInstance.search;
+      var queriesObject = CreateQueryObject(queries);
+      var mergedObject = {...variablesObject, ...queriesObject};
+      console.log("object", mergedObject);  
     }
+    else{
+      console.log("object", variablesObject);
+    }
+    
+    alert("Check console log to inspect printed object!");    
+  }
+  else {
+    alert("You must fill both boxes and provide a valid url domain .com!");    
+  }
 }
 
 createObject = (listKey, listValue) => {
