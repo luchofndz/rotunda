90b2ce274476e306a481f37205e2f1a07da07030
Luciano <luciano.fndz@gmail.com>
2020-02-28 00:07:01 -0300

    0006 - rotunda - modified view

--- zoo.js
+++ zoo.js
@@ -6,7 +6,7 @@ class Zoo {
     this.animalCollection.push(animal)
     return this.animalCollection;
   }
-  createAnimal = (kind) => {
+  createAnimal(kind) {
     var aniaml = new Animmal;
     animal.animalSound("grr");
     animal.speak("this is a test message");
@@ -18,19 +18,25 @@ class Animal {
   constructor() {
     this.animalWord = " Animal Sound ";
   }
-  animalSound = (sound) => {
+  animalSound(sound) {
     this.animalSound = sound;
   }
-  speak = (inputText) => {
+  speak(inputText) {
     var output = inputText.split(" ").join(this.animalWord);
     alert(output);
   }
 }
 
 submit = () => {
-  var zoo = new Zoo;
-  const textMessage = document.getElementById("").value;
+  //var zoo = new Zoo;
   //const textMessage = document.getElementById("").value;
+  //const textMessage = document.getElementById("").value;
+  var e = document.getElementsByClassName("dropdown-selection");
+  //var e = document.getElementById("dropdown-selection");
+  var result = e.options[e.selectedIndex].value;
+  console.log("result", result);
+  alert(result); //ID002
+  //$(".dropdown-toggle").dropdown();
 }
 
 /*
