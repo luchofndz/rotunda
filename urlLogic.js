6a27e43e27a57e47e19930721042f7a7d40d8aa5
Luciano <luciano.fndz@gmail.com>
2020-02-29 12:52:43 -0300

    0027 - rotunda - cleaned code

--- urlLogic.js
+++ urlLogic.js
@@ -1,27 +1,26 @@
 getInputValues = () => {
-  
   //Get input values
   const urlFormat = document.getElementById("form-format").value;
   const urlValues = document.getElementById("form-value").value;
 
   if (urlFormat && urlValues) {
     //Array format
-    var parserUrlFormat = document.createElement("a"); 
+    let parserUrlFormat = document.createElement("a"); 
     parserUrlFormat.href = urlFormat; 
-    var formatNotFiltered = parserUrlFormat.pathname.split('/');
+    let formatNotFiltered = parserUrlFormat.pathname.split('/');
 
     //Array values
-    var parserUrlInstance = document.createElement("a"); 
+    let parserUrlInstance = document.createElement("a"); 
     parserUrlInstance.href = urlValues;
-    var valuesNotFiltered = parserUrlInstance.pathname.split('/');
+    let valuesNotFiltered = parserUrlInstance.pathname.split('/');
     
     //Delete white spaces, colon and constant variables
-    var arrayKeys = [];
-    var arrayValues = [];
+    let arrayKeys = [];
+    let arrayValues = [];
     for (i = 0; i < formatNotFiltered.length; i++) {
       if (formatNotFiltered[i] !=='' && formatNotFiltered[i].charAt(0) === ':') {
         arrayKeys.push(formatNotFiltered[i].substring(1));
-        if( valuesNotFiltered[i] && valuesNotFiltered[i] !=='' ) {
+        if (valuesNotFiltered[i] && valuesNotFiltered[i] !=='') {
           arrayValues.push(valuesNotFiltered[i]);
         }
         else {
@@ -29,17 +28,16 @@ getInputValues = () => {
         }
       }
     }
-
-    var variablesObject = createObject(arrayKeys, arrayValues);
+    const variablesObject = createObject(arrayKeys, arrayValues);
 
     //Queries
-    if(parserUrlInstance.search) {
-      var queries = parserUrlInstance.search;
-      var queriesObject = CreateQueryObject(queries);
-      var mergedObject = {...variablesObject, ...queriesObject};
+    if (parserUrlInstance.search) {
+      let queries = parserUrlInstance.search;
+      const queriesObject = CreateQueryObject(queries);
+      const mergedObject = {...variablesObject, ...queriesObject};
       console.log("object", mergedObject);  
     }
-    else{
+    else {
       console.log("object", variablesObject);
     }
     
@@ -51,7 +49,7 @@ getInputValues = () => {
 }
 
 createObject = (listKey, listValue) => {
-  objectKValue = new Object;
+  let objectKValue = {};
   for (i = 0; i < listKey.length; i++) {
     let key = listKey[i];
     let value = listValue[i];
@@ -63,10 +61,10 @@ createObject = (listKey, listValue) => {
 }
 
 CreateQueryObject = (queries) => {
-  let queriesObject = new Object;
-  var queriesFiltered = queries.replace(/^\?/, '').split('&');
+  let queriesObject = {};
+  let queriesFiltered = queries.replace(/^\?/, '').split('&');
   queriesFiltered.map((item, index) => {
-    var split = item.split('=');
+    let split = item.split('=');
     queriesObject[split[0]] = split[1];
   });
   return (
